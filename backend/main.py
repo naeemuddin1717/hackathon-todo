c1f0cdfff1fe8c8c2869e9b0f8a0387ec0ed73b8
naeemuddin1717 <naeemuddin1717@gmail.com>
2025-12-31 23:07:03 +0500

    third commit

--- backend/main.py
+++ backend/main.py
@@ -8,8 +8,11 @@ app = FastAPI(title="Todo App (FastAPI + Neon + JWT + Gemini)")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000",
-        "http://127.0.0.1:3000",],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "https://hackathon-todo-iota.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
